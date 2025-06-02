d86854cf1e5e9c2ea40d1738ff6ca893e78ff81f
sametcl2 <sametsahinn01@gmail.com>
2025-06-02 12:57:15 +0300

    fix style

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,17 +45,21 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx("relative", GeistSans.variable, GeistMono.variable)}
+      className={cx(
+        "relative w-screen",
+        GeistSans.variable,
+        GeistMono.variable
+      )}
       suppressHydrationWarning
     >
       <GoogleTagManager
         gtmId="G-XTS218RD77"
         gtmScriptUrl="https://www.googletagmanager.com/gtag/js?id=G-XTS218RD77"
       />
-      <body className="h-full xlg:h-screen">
+      <body className="dark:bg-neutral-950">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="absolute top-0 z-[-2] w-screen h-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-30%,rgba(88,86,195,0.4),rgba(255,255,255,0))] text-black dark:text-white dark:bg-neutral-950"></div>
           <Main>{children}</Main>
-          <div className="absolute top-0 z-[-2] h-full w-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-30%,rgba(88,86,195,0.4),rgba(255,255,255,0))] text-black dark:text-white dark:bg-neutral-950"></div>
         </ThemeProvider>
       </body>
     </html>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <section>
       <div className="mb-8">
-        <h1 className="text-3xl font-semibold mb-2 tracking-tight bg-gradient-to-r from-blue-500 to-purple-400  inline-block text-transparent bg-clip-text">
+        <h1 className="text-3xl font-semibold mb-2 tracking-tight bg-gradient-to-r from-blue-400 to-blue-500 inline-block text-transparent bg-clip-text">
           My Projects
         </h1>
         <p className="text-sm mb-2">The projects that I want to mention</p>
